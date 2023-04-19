33a81566e7035b75093fcae01f1640799dc90d70
olilinvar <olilinvar@gmail.com>
2023-04-19 11:17:55 +0200

    update

--- Game_or_Die/script.js
+++ Game_or_Die/script.js
@@ -1,47 +1,78 @@
+/*
+Mark all invalid text and symbols inside the editor if error appears on submit
+
+Display a fancy error message with errMsg and errField class instead of the titleErr and contentErr id's 
+using class.add inside the if's and else's of checkContent()
+
+errField makes the field red to indicate error in said field
+
+add sorting buttons on the viewer page
+sort by date and popularity
+
+
+*/
+
+
 var editor;
+const allPosts = [];
 
-
-document.getElementById("submit-btn").addEventListener("click", checkContent, callGetData);
-
+document.getElementById("submit-btn").addEventListener("click", callGetData);
 
 function callGetData() {
-    var data = editor.getData();
-    console.log(data);
-    var contentInput = data;
+    const data = editor.getData();
+    const contentInput = data;
     checkContent(contentInput);
 };
 
 function checkContent() {
-    let titleInput = document.getElementById("title").value.trim();
-    let contentInput = editor.getData().trim();
+    const titleInput = String(document.getElementById("title").value.trim());
+    const contentInput = String(editor.getData().trim()/*.replace(/<p[^>]*>/gi, '').replace(/<\/p>/gi, '')*/);
 
-    let titleIsValid = titleInput.length > 0 && /^[a-zA-Z0-9 ]+$/.test(titleInput);
-    let contentIsValid = contentInput.length > 0 && !/^\s*$/.test(contentInput);
+    const titleIsValid = /*titleInput.length > 0 &&*/ /^[a-zA-Z0-9 ]+$/.test(titleInput);
+    const contentIsValid = /*contentInput.length > 0 &&*/ !/^\s*$/.test(contentInput);
 
     if (!titleIsValid) {
-        document.getElementById("title-error").textContent = "Title is missing or invalid.";
+        document.getElementById("titleErr").textContent = "Title is missing or invalid.";
+        console.log("titleIsValid = " + titleIsValid)
     } else {
-        document.getElementById("title-error").textContent = "";
+        document.getElementById("titleErr").textContent = "";
+        console.log("titleIsValid = " + titleIsValid)
     }
 
     if (!contentIsValid) {
-        document.getElementById("content-error").textContent = "Content is missing or invalid.";
+        document.getElementById("contentErr").textContent = "Content is missing or invalid.";
+        console.log("contentIsValid = " + contentIsValid)
     } else {
-        document.getElementById("content-error").textContent = "";
+        document.getElementById("contentErr").textContent = "";
+        console.log("contentIsValid = " + contentIsValid)
     }
 
     if (titleIsValid && contentIsValid) {
-        submitContent();
-
-        console.log(titleInput);
-        console.log(contentInput );
+        submitContent(titleInput, contentInput);
+        // updateUI();
     }
 }
 
+function submitContent(title, content) {
+    console.log("submitContent() has been executed");
+    const newPost = { title, content }
+    allPosts.push(newPost);
 
-function submitContent() {
-    console.log("submitContent has been executed");
-} 
+    try {
+        localStorage.setItem("posts", JSON.stringify(allPosts));
+    }
+    catch (error) {
+        console.error("Error saving post to local storage:", error);
+    }
+
+    console.log(allPosts);
+
+}
+
+
+
+
+
 
 
 
