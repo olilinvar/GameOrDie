9de468340ebbce824e4a9de8ca6a31831dab999b
olilinvar <olilinvar@gmail.com>
2023-05-02 14:14:08 +0200

    update

--- Game_or_Die/ActiveCode/script.js
+++ Game_or_Die/ActiveCode/script.js
@@ -9,7 +9,6 @@ errField makes the field red to indicate error in said field
 add sorting buttons on the viewer page
 sort by date and popularity
 
-
 */
 
 
@@ -18,6 +17,14 @@ const posts = [];
 
 const validateTitle = (title) => {
     return title.length > 1 && /^[a-zA-Z0-9 ]+$/.test(title);
+    /*
+    This function takes a parameter representing my title and returns true if 
+    the title.length is greater than 1 and if the title passes the regex test.
+    Otherwise it returns false.
+
+    I can pass a value for the function to validate at any time
+    The exact same explenation applies to the validateContent() function below
+    */
 }
 
 const validateContent = (content) => {
@@ -29,39 +36,50 @@ const savePost = (title, content, backupContent) => {
         title,
         content: content || backupContent
     };
+    //Declares the newPost object containing the value of title, content or the backupContent parameters
 
     posts.push(newPost);
+    //The newPost object is pushed to the posts[] array
 
     try {
-        localStorage.setItem("storedPosts", JSON.stringify(posts));
+        localStorage.setItem("stringifiedPosts", JSON.stringify(posts));
         console.log("Post successfully saved in local storage")
+        //Tries to store a stringified version of the posts[] array under the name of stringifiedPosts
     } catch (error) {
         console.error("Error saving post to local storage:", error);
+        //Logs an error message if localStorage.setItem fails
     }
 }
 
 const processForm = () => {
     const titleInput = String(document.getElementById("title").value.trim());
     const contentInput = String(editor.getData().trim());
+    //editor.getData() is from the ckeditor library, it gets the input data from ckeditor's textarea
     const backupContent = String(document.getElementById("editor").value.trim());
+    //The backupContent variable is a fallback solution in case the ckeditor cdn servers are down
 
     const isTitleValid = validateTitle(titleInput);
     const isContentValid = validateContent(contentInput);
+    //The isValid variables are booleans used for validation and error handling.
+    //Their value is given based on what the validating functions return
 
     if (!isTitleValid) {
         document.getElementById("titleErr").textContent = "Title is missing or invalid.";
-        console.log("isTitleValid = "+isTitleValid)
+        console.log("isTitleValid = " + isTitleValid)
+        //If title is invalid: display error and log isTitleValid value
     } else {
         document.getElementById("titleErr").textContent = "";
-        console.log("isTitleValid = "+isTitleValid)
+        console.log("isTitleValid = " + isTitleValid)
+        //If title is valid: display nothing and log isTitleValid value
+        //The same explenation apply to the if-else statement below
     }
 
     if (!isContentValid) {
         document.getElementById("contentErr").textContent = "Content is missing or invalid.";
-        console.log("isContentValid = "+isContentValid)
+        console.log("isContentValid = " + isContentValid)
     } else {
         document.getElementById("contentErr").textContent = "";
-        console.log("isContentValid = "+isContentValid)
+        console.log("isContentValid = " + isContentValid)
     }
 
 
@@ -72,12 +90,18 @@ const processForm = () => {
 
     if (isTitleValid && !isContentValid && validateContent(backupContent)) {
         savePost(titleInput, null, backupContent);
+        /*
+        The validateContent function is called in the conditions to check if backupContent is valid
+        If the title and backupContent is valid while content is invalid we execute the savePost function passing the valid title and backupContent
+        This is a fallback solution in case the ckeditor cdn servers are down
+        */
     }
 }
 
 const updateUI = () => {
-    localStorage.getItem("storedPosts");
-    storedPosts;
+    const storedPosts = JSON.parse(localStorage.getItem("stringifiedPosts"));
+    console.log(storedPosts)
+    //This function is called from allPosts.html retrive the stringifiedPosts array from localStorage
 }
 
 const callGetData = () => {
